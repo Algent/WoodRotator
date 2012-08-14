f31525c0976b7441d2cbccc67c5d708aff0c69e4
Algent <algent@gmail.com>
2012-08-15 01:47:18 +0200

    V1.1 : Added Worldguard support.

--- src/main/java/eu/algent/WoodRotator/WoodRotator.java
+++ src/main/java/eu/algent/WoodRotator/WoodRotator.java
@@ -1,17 +1,23 @@
 package eu.algent.WoodRotator;
 
-import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.event.EventHandler;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+
 public class WoodRotator extends JavaPlugin implements Listener {
+    private Boolean worldguard = false;
+    private WorldGuardPlugin wgPlugin;
 
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
+        // Check for WorldGuard
+        worldguard = getServer().getPluginManager().getPlugin("WorldGuard") != null;
+        if (worldguard) wgPlugin = getWorldguard();
+        // Init listener
+        getServer().getPluginManager().registerEvents(new WoodRotatorListener(this), this);
         getLogger().info(getName() + " has been enabled.");
     }
 
@@ -19,73 +25,20 @@ public class WoodRotator extends JavaPlugin implements Listener {
         getLogger().info(getName() + " has been disabled.");
     }
 
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        // Check if left click + sneaking
-        if (!(event.getAction().equals(Action.LEFT_CLICK_BLOCK) 
-                && event.getPlayer().isSneaking())) return;
-        // Check if clicked block is wood
-        Block block = event.getClickedBlock();
-        if (block.getType() != Material.LOG) return;
-        // Check player permissions
-        if (!event.getPlayer().hasPermission("woodrotator.rotate")) return;
-        Boolean barkFace = event.getPlayer().hasPermission("woodrotator.bark");
-        // Rotate Wood
-        Byte data = block.getData();
-        switch (data) {
-        // Oak
-        case 0: 
-            data = 4;
-            break;
-        case 4: 
-            data = 8;
-            break;
-        case 8: 
-            data = 12;
-            if(barkFace) break;
-        case 12: 
-            data = 0;
-            break;
-        // Pine
-        case 1: 
-            data = 5;
-            break;
-        case 5: 
-            data = 9;
-            break;
-        case 9: 
-            data = 13;
-            if(barkFace) break;
-        case 13: 
-            data = 1;
-            break;
-        // Birch
-        case 2: 
-            data = 6;
-            break;
-        case 6: 
-            data = 10;
-            break;
-        case 10: 
-            data = 14;
-            if(barkFace) break;
-        case 14: 
-            data = 2;
-            break;
-        // Jungle
-        case 3: 
-            data = 7;
-            break;
-        case 7: 
-            data = 11;
-            break;
-        case 11: 
-            data = 15;
-            if(barkFace) break;
-        case 15: 
-            data = 3;
-            break;
+    private WorldGuardPlugin getWorldguard() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
+        if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
+            worldguard = false;
+            return null;
         }
-        block.setData(data);
+        return (WorldGuardPlugin) plugin;
+    }
+
+    public Boolean canRotateWg(Player player, Block block) {
+        if (!worldguard) return true;
+        if (wgPlugin.canBuild(player, block)) {
+            return true;
+        }
+        return false;
     }
 }
